8ae0043d0560760d2fed655205dc1e941b513b72
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-08-31 15:02:59 +0300

    HomeWork14

---
+++ HomeWork14/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите Имя");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите символ");
+        string symbol = Console.ReadLine();
+        int sideSymbol = 2;
+
+        for (int i = 0; i < name.Length + sideSymbol; i++)
+        {
+            Console.Write(symbol);
+        }
+
+        Console.WriteLine($"\n{symbol}{name}{symbol}");
+
+        for (int i = 0; i < name.Length + sideSymbol; i++)
+        {
+            Console.Write(symbol);
+        }
+    }
+}
